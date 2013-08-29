b010c35d3a26209746ef06fed33f33378ac9b40c
HomoWalrus <walrushere@gmail.com>
2013-08-29 20:18:19 +0400

    рандомный выбор конца и начала маршрута

--- transport_model/src/machines/Agent.java
+++ transport_model/src/machines/Agent.java
@@ -8,20 +8,61 @@ import java.util.*;
 
 public class Agent {
 	
-	private	koord nextPosition=new koord();
+	private koord nextPosition=new koord();
+	private int speed;
+	private Stack shortestpath = new Stack();
+	private int current;
+	private int  end;
+	private koord curPos;
 	
-	public Agent(){
+	public Agent(int sp, Stack shpath, int cur, int end, koord curPos){
 		
-		Stack shortestpath = new Stack();
-		int speed;
-		koord start;
-		koord end;
-		koord currentPosition;
-
+	
+		this.speed=sp;
+		this.shortestpath=shpath;
+		this.current=cur;
+		this.end=end;
+		this.curPos=curPos;
 	}
 	
-	public koord getNewPosition(){
-			return nextPosition;
+	public static int getRandom(int[] array) {
+	    int rnd = new Random().nextInt(array.length);
+	    return array[rnd];
 	}
+
+
+	public void setAgent(Agent agent){
+		
+		int i;
+		int length=global.kolPer;
+		int[] startarray=new int[length];
+		int[] endarray=new int[length];
+		int[] speedarray=new int[20];
+		
+		for(i=0;i<=length;i++)
+		{
+			startarray[i]=i;
+		}
+		
+		for(i=0;i<=length;i++)
+		{
+			endarray[i]=i;
+		}
+		
+		for(i=20;i<=40;i++)
+		{
+			speedarray[i]=i;
+		}
+		
+		this.current=getRandom(startarray);
+		this.end=getRandom(endarray);
+		this.speed=getRandom(speedarray);
+		
+	}
+	public koord getNewPosition(Agent agent){
+				
+			return this.nextPosition;
+	}
+	
 	
 }
